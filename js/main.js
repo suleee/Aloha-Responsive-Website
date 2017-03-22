991c197f9e3ffe37922a3387ade8f157604271c3
Jisu <jaejisu@gmail.com>
2017-03-22 12:27:31 -0700

    remove extra ready function

--- js/main.js
+++ js/main.js
@@ -12,12 +12,9 @@ $(function () {
       }
     }
   });
-});
 
 
-
-// items
-$(function () {
+  // items
   $(".item-list").flickity({
     cellAlign: 'center',
     contain: true,
@@ -26,44 +23,38 @@ $(function () {
     autoPlay: true,
     prevNextButtons: false
   });
-});
 
 
-// cart button
-$(function () {
+  // cart button
   var $cartTotal = 0;
-
   $('.counter').hide();
   $('.button').on('click', function (event) {
-event.preventDefault();
-$cartTotal++;
+    event.preventDefault();
+    $cartTotal++;
     $('.counter').text($cartTotal).show();
     console.log('.counter');
-
   });
-});
 
 
-
-
-// email button: '#submit=button ID'
-$('#submit').on('click', function (event) {
-  event.preventDefault();
-  var userEmail = $('input:text').val();
-  if (userEmail != '') {
-    var valid = validateEmail(userEmail);
-    if (valid) {
-      alert("Thanks for subscribing!");
+  // email button: '#submit=button ID'
+  $('#submit').on('click', function (event) {
+    event.preventDefault();
+    var userEmail = $('input:text').val();
+    if (userEmail != '') {
+      var valid = validateEmail(userEmail);
+      if (valid) {
+        alert("Thanks for subscribing!");
+      } else {
+        alert("Please include an '@' in email address. " + userEmail + " is missing an '@'.");
+      }
     } else {
-      alert("Please include an '@' in email address. " + userEmail + " is missing an '@'.");
+      alert("Please submit a valid email address.");
     }
-  } else {
-    alert("Please submit a valid email address.");
+  });
+
+//email
+  function validateEmail(email) {
+    var pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return pattern.test(email);
   }
-
 });
-
-function validateEmail(email) {
-  var pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return pattern.test(email);
-}
