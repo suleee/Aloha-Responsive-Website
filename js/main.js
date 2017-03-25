49daf2a609c856f1c0df6a7d1bc0bb0830cb8e92
Jisu <jaejisu@gmail.com>
2017-03-25 10:35:53 -0700

    add 'about' anchore point, scroll effect on logo, and add padding on the sign up pragraph

--- js/main.js
+++ js/main.js
@@ -6,12 +6,23 @@ $(function () {
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top - 90
+          scrollTop: target.offset().top - 78
         }, 500);
         return false;
       }
     }
   });
+  $('.logo').click(function () {
+    $('html,body').animate({
+      scrollTop: 0
+    }, 1000);
+  });
+  $('.about-scroll').click(function () {
+    $('html,body').animate({
+      scrollTop: 400
+    }, 1000);
+  });
+
 
 
   // items
@@ -52,7 +63,7 @@ $(function () {
     }
   });
 
-//email
+  //email
   function validateEmail(email) {
     var pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return pattern.test(email);
